b0101d5cbf0b1c5ddb74d6ede056844a6b27a608
Stanislav Čaja <cajastan@cvut.cz>
2019-03-28 19:21:51 +0100

    Add project files.

---
+++ BtlParser/IXmlStatementsParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtlParser
+{
+    interface IXmlStatementsParser
+    {
+        /// <summary>
+        /// Contains all the morphology statements
+        /// </summary>
+        IEnumerable<string> Morphology { get; }
+
+        /// <summary>
+        /// Contains all the rhythm statements
+        /// </summary>
+        IEnumerable<string> Rhythms { get; }
+    }
+}
---
+++ BtlParser/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\stany\\Downloads\\BaseXMLs\\Copy of R_T001.BA91M11.134.807.216.916 v1.0 T001 2012-01-01_1#BA91M11.134807216916.xml";
+            IXmlStatementsParser xml = new XmlStatementsParser(filePath);
+            xml.Morphology
+        }
+    }
+}
