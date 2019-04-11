5b246c5ade6174b137637f0d4964264ef785ae4c
Stanislav Čaja <cajastan@cvut.cz>
2019-04-11 14:19:19 +0200

    Add interfaces for xml pairs

---
+++ BtlParser/IXmlPair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtlParser
+{
+    interface IXmlPair
+    {
+        /// <summary>
+        /// The path of the current Xml
+        /// </summary>
+        string FilePath { get; set; }
+
+        /// <summary>
+        /// Contains statements of one xml
+        /// </summary>
+        IXmlStatementsParser Statements { get; set; }
+
+    }
+}
---
+++ BtlParser/IXmlPairer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtlParser
+{
+    interface IXmlPairer
+    {
+        /// <summary>
+        /// Contains all the morphology statements
+        /// </summary>
+        IEnumerable<IXmlPair> Pairs { get; }
+
+        /// <summary>
+        /// Fill the <see cref="Pairs"/> with the data from directory
+        /// </summary>
+        /// <param name="referenceDirectory"></param>
+        /// <param name="analysisDirectory"></param>
+        void MakePairs(string referenceDirectory, string analysisDirectory);
+    }
+}
